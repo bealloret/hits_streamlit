20c15db9d94cc260686afb6383b329890aa52b18
Beatriz Lloret <bealloret@gmail.com>
2023-11-01 20:30:48 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -22,12 +22,12 @@ st.image("https://t2.genius.com/unsafe/249x249/https%3A%2F%2Fimages.genius.com%2
          output_format="PNG",
          use_column_width=False,
          clamp=False,
-         output_height=200,
+         height=200,
          style="border-radius: 10px;"
          )
 
 # Display the sentence with the same size as the song title
-st.write("## Look at the features of this song:")
+st.markdown("## Look at the features of this song:")
 
 # Creating a sample plot for the example of popularity
 features = ['danceability', 'energy', 'explicit', 'duration_ms', 'year', 'key', 'loudness', 'mode', 'speechiness', 
