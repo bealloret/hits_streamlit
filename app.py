a2db8644556b2fa930b0ffd30ecd401fdabae78f
Beatriz Lloret <bealloret@gmail.com>
2023-11-02 11:55:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from additional_page import display_additional_page
 
 def main():
     st.sidebar.title('Navigation')
-    selection = st.sidebar.radio("Go to", ['Home', 'Set page'])
+    selection = st.sidebar.radio("Go to", ['Home', 'Setting page'], label="Choose a Page")
 
     if selection == 'Home':
         display_home_page()
-    elif selection == 'Set page':
+    elif selection == 'Setting page':
         display_additional_page()
 
 if __name__ == "__main__":
