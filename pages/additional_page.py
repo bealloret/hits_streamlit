69b8383406d8657fc520a388efa0bdbd65673997
Beatriz Lloret <bealloret@gmail.com>
2023-11-02 12:47:26 +0100

    Update additional_page.py

--- pages/additional_page.py
+++ pages/additional_page.py
@@ -28,10 +28,18 @@ def display_additional_page():
     elif feature_set == 'Set 3':
         features = ['liveness', 'valence', 'tempo', 'followers']
 
+    # Create input fields for the selected features
+    feature_values = {}
+    for feature in features:
+        if feature == 'year' or feature == 'key' or feature == 'mode':
+            feature_values[feature] = st.number_input(feature, key=feature)
+        else:
+            feature_values[feature] = st.slider(feature, key=feature)
+
     # Plot
     fig, ax = plt.subplots(figsize=(8, 5))
     colors = ['blue', 'green', 'red', 'purple', 'orange']
-    popularity_scores = [0.5, 0.7, 0.3, 0.6, 0.4]  # Adjust these values with actual data
+    popularity_scores = [feature_values[feature] for feature in features]
 
     bars = ax.bar(features, popularity_scores, color=colors)
 
@@ -48,33 +56,30 @@ def display_additional_page():
 
     st.pyplot(fig)
 
-    # Create input fields for the selected features
-    for feature in features:
-        if feature == 'year' or feature == 'key' or feature == 'mode':
-            st.number_input(feature, key=feature)
-        else:
-            st.slider(feature, key=feature)
-
     # Create a DataFrame with the user inputs
     new_song = pd.DataFrame({
         'artist': [artist],
         'genre': [genre],
-        'danceability': [0.5],  # Default value for danceability
-        'energy': [0.7],  # Default value for energy
-        'explicit': [0.3],  # Default value for explicit
-        'duration_ms': [0.6],  # Default value for duration_ms
-        'year': [0.4],  # Default value for year
-        'key': [0.8],  # Default value for key
-        'loudness': [0.2],  # Default value for loudness
-        'mode': [0.5],  # Default value for mode
-        'speechiness': [0.7],  # Default value for speechiness
-        'acousticness': [0.3],  # Default value for acousticness
-        'instrumentalness': [0.6],  # Default value for instrumentalness
-        'liveness': [0.4],  # Default value for liveness
-        'valence': [0.8],  # Default value for valence
-        'tempo': [0.2],  # Default value for tempo
-        'followers': [0.5]  # Default value for followers
+        'danceability': [feature_values.get('danceability', 0.5)],  # Default value for danceability
+        'energy': [feature_values.get('energy', 0.7)],  # Default value for energy
+        'explicit': [feature_values.get('explicit', 0.3)],  # Default value for explicit
+        'duration_ms': [feature_values.get('duration_ms', 0.6)],  # Default value for duration_ms
+        'year': [feature_values.get('year', 0.4)],  # Default value for year
+        'key': [feature_values.get('key', 0.8)],  # Default value for key
+        'loudness': [feature_values.get('loudness', 0.2)],  # Default value for loudness
+        'mode': [feature_values.get('mode', 0.5)],  # Default value for mode
+        'speechiness': [feature_values.get('speechiness', 0.7)],  # Default value for speechiness
+        'acousticness': [feature_values.get('acousticness', 0.3)],  # Default value for acousticness
+        'instrumentalness': [feature_values.get('instrumentalness', 0.6)],  # Default value for instrumentalness
+        'liveness': [feature_values.get('liveness', 0.4)],  # Default value for liveness
+        'valence': [feature_values.get('valence', 0.8)],  # Default value for valence
+        'tempo': [feature_values.get('tempo', 0.2)],  # Default value for tempo
+        'followers': [feature_values.get('followers', 0.5)]  # Default value for followers
     })
+
+    # Display the predicted popularity
+    predicted_popularity = loaded_model.predict(new_song)
+    st.write(f"Predicted popularity: {predicted_popularity}")
 
 if __name__ == "__main__":
     display_additional_page()
