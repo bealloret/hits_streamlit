5f0e388cefd1c1a1b3f5d6b2df34d6c5a7ee5499
Beatriz Lloret <bealloret@gmail.com>
2023-11-02 11:26:33 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -77,7 +77,7 @@ def display_home_page():
     genre = st.text_input("genre")
 
     st.write("Danceability:")
-    energy = st.slider("Adjust danceability", min_value=0.0, max_value=1.0, step=0.01)
+    danceabilty = st.slider("Adjust danceability", min_value=0.0, max_value=1.0, step=0.01)
 
 
     st.write("Energy:")
