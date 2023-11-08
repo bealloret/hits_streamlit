e3925a10f2bedee4a38a56fa2b4497d2ef83e0b6
Beatriz Lloret <bealloret@gmail.com>
2023-11-08 10:21:49 +0100

    Update Song_similarity_search_page.py

--- pages/Song_similarity_search_page.py
+++ pages/Song_similarity_search_page.py
@@ -110,7 +110,6 @@ similar_songs = calculate_similarities(data_for_recommender, input_df)
 # Display the results
 st.write("Top 5 similar songs:")
 st.write(similar_songs)
-st.write(input_df)
 
 if __name__ == "__main__":
     display_song_similarity_search_page()
